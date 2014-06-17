7a412eb1ab077b732ba3fdfd22a609bc455a5ca3
yosssi <yoshida.keiji.84@gmail.com>
2014-06-17 18:21:25 +0900

    Add store/config_test.go

--- store/config_test.go
+++ store/config_test.go
@@ -1,1 +1,21 @@
 package store
+
+import (
+	"testing"
+
+	"github.com/yosssi/boltstore/shared"
+)
+
+func TestConfig_setDefault(t *testing.T) {
+	config := Config{}
+	config.setDefault()
+	if config.SessionOptions.Path != shared.DefaultPath {
+		t.Errorf("config.SessionOptions.Path should be %s (actual: %s)", shared.DefaultPath, config.SessionOptions.Path)
+	}
+	if config.SessionOptions.MaxAge != shared.DefaultMaxAge {
+		t.Errorf("config.SessionOptions.MaxAge should be %s (actual: %s)", shared.DefaultMaxAge, config.SessionOptions.MaxAge)
+	}
+	if string(config.DBOptions.BucketName) != shared.DefaultBucketName {
+		t.Errorf("config.SessionOptions.BucketName should be %+v (actual: %+v)", shared.DefaultBucketName, config.DBOptions.BucketName)
+	}
+}
